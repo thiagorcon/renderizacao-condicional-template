9c7570b17f162313b4fda8470df8a48a477d0f84
yuzo <yuzokamoto@gmail.com>
2023-04-10 09:47:23 -0300

    update

--- src/components/TelaCadastro/styled.js
+++ src/components/TelaCadastro/styled.js
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  margin-bottom: 200px;
+
+  h1 {
+    margin: 32px;
+  }
 `;
 
 export const Form = styled.div`
-  width: 40vw;
-  height: 60vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -36,16 +38,16 @@ export const SendButton = styled.button`
 `;
 
 export const BackToLoginButton = styled.button`
-    background-color: white;
-    border-radius: 8px;
-    cursor: pointer;
-    width: 200px;
-    padding: 10px;
-    :hover{
-        background-color: black;
-        color: white;
-    }
-`
+  background-color: white;
+  border-radius: 8px;
+  cursor: pointer;
+  width: 200px;
+  padding: 10px;
+  :hover {
+    background-color: black;
+    color: white;
+  }
+`;
 
 export const StyledLabel = styled.label`
   width: 300px;
--- src/components/TelaPrincipal/styled.js
+++ src/components/TelaPrincipal/styled.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
 export const Div = styled.div`
-    height: 90vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 200px;
 
-    p {
-      margin: 8px;
-    }
+  p {
+    margin: 8px;
+  }
 
-    button {
-      background-color: #ff5f59;
-      border-radius: 8px;
-      padding: 16px;
-      width: 200px;
-      border: none;
-      color: white;
-      margin-top: 32px;
-      cursor: pointer;
-      font-weight: bold;
-    }
-`
+  button {
+    background-color: #ff5f59;
+    border-radius: 8px;
+    padding: 16px;
+    width: 200px;
+    border: none;
+    color: white;
+    margin-top: 32px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+`;
 
 export const Titulo = styled.h1`
-    margin: 16px;
-`
+  margin: 16px;
+`;
